b1c58bf7bbc00b4af20bda5c94feaa1aeea58574
Ritvik Girish Girish <94665657+Ritvik-Girish@users.noreply.github.com>
2022-05-13 15:56:07 +0000

    P7

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,18 @@
 # Strings
-
-text = "X-DSPAM-Confidence:    0.8475"
+l = None
+s = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+        if l is None or l < num:
+            l = num
+        if s is None or s > num:
+            s = num
+    except:
+        print ("Invalid input")
+        continue
+print ("Maximum is",l)
+print ("Minimum is",s)
