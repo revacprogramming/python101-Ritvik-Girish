cb3aa4e7bfe6b1b5d2a4285c8a972ffc994347fe
Ritvik Girish Girish <94665657+Ritvik-Girish@users.noreply.github.com>
2022-05-19 16:11:56 +0000

    P9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,19 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+l = None
+s = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+        if l is None or l < num:
+            l = num
+        if s is None or s > num:
+            s = num
+    except:
+        print ("Invalid input")
+        continue
+print ("Maximum is",l)
+print ("Minimum is",s)
