1d1d6f2b139768a1087c4ae76bea24594117a4b1
Ritvik Girish Girish <94665657+Ritvik-Girish@users.noreply.github.com>
2022-05-23 05:56:57 +0000

    P3

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,14 +1,5 @@
 # Variables, Expressions & Statements
-def Input(hour , rate) :
-  hour = input("Enter Hours:")
-  rate = input("Enter Rate :")
-  return hour,rate
-
-def Calc(h,r):
-  amt = h * r
-  return amt
-
-def Output(amt):
-  amount = float(hrs) * float(rate)
-  print("Pay: ",amount)
-  
+hrs = input("Enter Hours:")
+rate = input("Enter Rate :")
+amount = float(hrs) * float(rate)
+print("Pay: ",amount)
