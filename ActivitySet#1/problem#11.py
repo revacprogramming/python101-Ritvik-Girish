6e9d09df058f28bcec058d6ed167d4480c8018f9
Ritvik Girish Girish <94665657+Ritvik-Girish@users.noreply.github.com>
2022-05-31 14:40:22 +0000

    P11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,19 @@
 # Tuples
-
-filename = "dataset/mbox-short.txt"
+l = None
+s = None
+def 
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+        if l is None or l < num:
+            l = num
+        if s is None or s > num:
+            s = num
+    except:
+        print ("Invalid input")
+        continue
+print ("Maximum is",l)
+print ("Minimum is",s)
