553ef72adf6a8d2324ae7f00126f6924a1245c58
Ritvik Girish Girish <94665657+Ritvik-Girish@users.noreply.github.com>
2022-06-06 07:01:38 +0000

    P8 fixed

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,18 +1,28 @@
-# Files
-l = None
-s = None
-while True:
-    num = input("Enter a number: ")
-    if num == "done":
-        break
-    try:
-        num = int(num)
-        if l is None or l < num:
-            l = num 
-        if s is None  or s > num:
-            s = num
-    except:
-        print ("Invalid input")
-        continue
-print ("Maximum is",l)
-print ("Minimum is",s)
+# Use the file name mbox-short.txt as the file name
+def myinput():
+    fname = input("Enter file name")
+    return fname
+
+def file(fname):
+  f=open(fname)
+  count = 0
+  sum=0
+  for line in f:
+      if line.startswith('X-DSPAM-Confidence'):
+          count = count + 1
+          x=float(line[20::1])
+          sum=sum+x
+  print('There were', count, 'subject lines in', "dataset/mbox-short.tx")
+  avg=sum/(count)
+  return avg
+           
+def output(avg):            
+  print("Average spam confidence: ",avg)
+  
+
+def main():
+    fn = myinput()
+    av = file(fn)
+    output(av)
+    
+main()
